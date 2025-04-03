fb8a540ec6a0f6499a3dc87b872213b1035bec02
fehmm <fefemendesfmm@gmail.com.br>
2025-04-03 20:13:30 -0300

    ola

--- ola_mundo/ola_mundo.c
+++ ola_mundo/ola_mundo.c
@@ -1,46 +1,7 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(int argc, char* argv[]){
+printf("hello, world!\n");
 
-    float altura, peso, imc; 
-
-printf("calculadora IMC\n");
-
-// imprimeuma mensagem a ler 
-// um valor float na variavel altura
-prinf("digite a altura: ");
-acanf("%f", &altura);
-
-printf("digite o peso: ");
-scanf("%f", &peso);
-
-imc = peso / (altura * altura);
-
-printf("IMC  = %.2F\n", imc);
-
-if(imc <= 18,5){
-    printf("baixo peso\n");
+return 0
 }
-
-if(imc > 18,5 && imc <= 24.9){
-    prinf ("intervalo normal\n");
-}
-
-if(imc > 24.9 && imc <= 29.9){
-    printf("sobrepeso\n"); 
-}
-
-if(imc > 29.9 && imc <= 34.9){
-    printf("obesidade I\n");
-}
-
-if(imc > 34.9 && imc <= 39.9){
-    printf("obesidade II\n");
-}
-
-if(imc > 39.9){
-    printf("obesidade III\n");
-}
-return 0;
-} 
