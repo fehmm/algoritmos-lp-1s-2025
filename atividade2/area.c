476ee1684e2c5494141da40662f842422fdfaf95
fehmm <fefemendesfmm@gmail.com.br>
2025-04-04 17:30:36 -0300

    atividade

--- atividade2/area.c
+++ atividade2/area.c
@@ -1,42 +1,75 @@
 #include <stdio.h>
 #include <math.h>
 
-int main( int argc, char* argv[]){
+int main(int argc, char* argv[]){
+    
+    char opcao;
+    float r, D, d, L, b, h, B, A;
 
-    char opcao; 
-    float r, D, d, r, 
+    printf("area.c\n");
+    printf("Escolha uma opcao: \n");
+    printf("c - Circulo\n");
+    printf("l - Losango\n");
+    printf("q - Quadrado\n");
+    printf("r - Retangulo\n");
+    printf("t - Triangulo\n");
+    printf("z - Trapezio\n");
 
-printf("area.c\n");
-printf("escolha uma opcao: \n");
-printf("c - circulo\n");
-printf("1 - losango\n");
-printf("q - quadrado\n"); 
-printf("r - retangulo\n");
-printf("t - triangulo\n");
-printf("z - trapezio\n"); 
+    scanf("%c", &opcao);
 
-acanf("%c", &opcao);
+    switch(opcao){
 
-switch (opcao){
-
-case 'c':
-    printf("circulo\n");
-    break;
-
-    case'1':
-    printf("losango\n");
-    break;
-
-    case 'q'
-    printf("quadrado\n");
-    break;
-
-
-default:
-   printf("opcao invalida\n")
-   A = 0; 
-    break;
-}
-
-
+        case 'c':
+            printf("Circulo\n");
+            printf("Digite o valor do raio (r): ");
+            scanf("%f", &r);
+            A = M_PI * powf(r, 2);
+            break;
+        case 'l':
+            printf("Losango\n");
+            printf("Digite o valor da diagonal maior (D): ");
+            scanf("%f", &D);
+            printf("Digite o valor da diagonal menor (d): ");
+            scanf("%f", &d);
+            A = (D * d)/2;
+            break;
+        case 'q':
+            printf("Quadrado\n");
+            printf("Digite o valor do lado (L): ");
+            scanf("%f", &L);
+            A = L * L;
+            break;
+        case 'r':
+            printf("Retangulo\n");
+            printf("Digite o valor da base (b): ");
+            scanf("%f", &b);
+            printf("Digite o valor da altura (h): ");
+            scanf("%f", &h);
+            A = b * h;
+            break;
+        case 't':
+            printf("Triangulo\n");
+            printf("Digite o valor da base (b): ");
+            scanf("%f", &b);
+            printf("Digite o valor da altura (h): ");
+            scanf("%f", &h);
+            A = (b * h)/2;
+            break;
+        case 'z':
+            printf("Trapezio\n");
+            printf("Digite o valor da base maior (B): ");
+            scanf("%f", &B);
+            printf("Digite o valor da base menor (b): ");
+            scanf("%f", &b);
+            printf("Digite o valor da altura (h): ");
+            scanf("%f", &h);
+            A = ((B + b)*h)/2;
+            break;
+        default:
+            printf("Opcao invalida\n");
+            A = 0;
+            break;
+    }
+    printf("A = %.2f\n", A);
+    return 0;
 }
