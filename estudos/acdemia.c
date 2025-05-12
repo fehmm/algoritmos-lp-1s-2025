f715ad9ac846103fbf03636e9de2dace97914cd9
fehmm <fefemendesfmm@gmail.com.br>
2025-05-12 19:10:43 -0300

    estudos

--- estudos/acdemia.c
+++ estudos/acdemia.c
@@ -4,10 +4,14 @@ int main(int argc, char const *argv[])
 {
     int idade = 18;
     float peso = 46.3;
-    char sexo = 'F';
+    char sexo = 'f';
     double pagamento = 99.99;
 
     printf("\n se estou com %d anos, peso %.2f kg e sou do sexo %c, o pagamento da academia sera de %.2lf reais \n" , idade, peso, sexo, pagamento);
     
     return 0;
 }
+   
+// estudos 
+
+// TIPOS DE DADOS 
