6d60073a006db76c1d58a16f0b9c706a4479dba1
fehmm <fefemendesfmm@gmail.com>
2025-08-05 19:48:44 -0300

    first commit

---
+++ ja/adivinha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ int main () {
+
+    printf("***********************************\n");
+
+    printf("* BEM-VINDO AO JODO DE ADIVINHACAO *\n");
+
+    printf("***********************************\n");
+
+    int chute;
+    int numerosecret = 42;
+
+    printf("qual e o seu chute?");
+    scanf("%d", &chute);
+    printf("voce chutou o numero %d", chute);
+ }
